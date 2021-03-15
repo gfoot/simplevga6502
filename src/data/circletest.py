11ef69ad40a548c102932627d5b2f13fa130df7c
George Foot <george.foot@gmail.com>
2021-03-15 00:56:02 +0000

    Changed Pi Day 2021 test program to use better circle drawing algorithm

--- src/data/circletest.py
+++ src/data/circletest.py
@@ -1,30 +1,26 @@
 
 def octant(radius):
 	result = [""] * radius
-	y = 0
-	ysq = 0
-	x = radius
-	residue = x
+	y1 = 0
+	x1 = radius
+	residue = x1
 
-	k = []
-	n = 0
-	m = radius-1
+	x2 = 0
+	y2 = radius-1
 
-	while y <= m:
-		result[y] = "*" * x
-		ysq += y
-		residue -= y
-		y += 1
-		ysq += y
-		residue -= y
-		n += 1
+	while y1 <= y2:
+		result[y1] = "*" * x1
+		residue -= y1
+		y1 += 1
+		residue -= y1
+		x2 += 1
 
 		if residue < 0:
-			residue += x
-			x -= 1
-			residue += x
-			result[m] = "*" * n
-			m -= 1
+			residue += x1
+			x1 -= 1
+			residue += x1
+			result[y2] = "*" * x2
+			y2 -= 1
 
 	print(len(result), len(result[0]))
 	for line in result:
--- src/piday2021.s
+++ src/piday2021.s
@@ -26,314 +26,329 @@ reset:
   lda #%0000
   jsr vram_clear
 
-	jsr wait_button
+  lda #0
+  sta ZP_RANDOMCOLOURS_FLAG
 
-	lda #0
-	sta ZP_RANDOMCOLOURS_FLAG
+  lda #0
+  sta ZP_BGCOLOR
 
-	lda #0
-	sta ZP_BGCOLOR
+  lda #1
+  sta ZP_FGCOLOR
 
-	lda #1
-	sta ZP_FGCOLOR
+  ldx #0
+  stx DRAWSTRING_X
+  ldy #2
+  sty DRAWSTRING_Y
+  jsr drawstringimm
+  .asciiz "to /r/beneater"
 
-	ldx #0
-	stx DRAWSTRING_X
-	ldy #2
-	sty DRAWSTRING_Y
-	jsr drawstringimm
-	.asciiz "to /r/beneater"
+  lda #4
+  sta ZP_FGCOLOR
 
-	jsr wait_button
+  ldx #130
+  stx DRAWSTRING_X
+  ldy #175
+  sty DRAWSTRING_Y
+  jsr drawstringimm
+  .asciiz "from"
 
-	lda #4
-	sta ZP_FGCOLOR
+  ldx #116
+  stx DRAWSTRING_X
+  ldy #185
+  sty DRAWSTRING_Y
+  jsr drawstringimm
+  .asciiz "/u/gfoot360"
 
-	ldx #130
-	stx DRAWSTRING_X
-	ldy #175
-	sty DRAWSTRING_Y
-	jsr drawstringimm
-	.asciiz "from"
-
-	ldx #116
-	stx DRAWSTRING_X
-	ldy #185
-	sty DRAWSTRING_Y
-	jsr drawstringimm
-	.asciiz "/u/gfoot360"
-
-	jsr wait_button
+  jsr wait_button
 
 
-	lda #1
-	sta ZP_FGCOLOR
-	lda #0
-	sta ZP_BGCOLOR
-	lda #96
-	ldx #$20
-	ldy #$20
-	jsr draw_circle
+  lda #1
+  sta ZP_FGCOLOR
+  lda #0
+  sta ZP_BGCOLOR
+  lda #96
+  jsr draw_circle
 
-	jsr wait_button
+  jsr wait_button
 
-	lda ZP_FGCOLOR
-	sta ZP_BGCOLOR
-	lda #0
-	sta ZP_FGCOLOR
-	lda #80
-	ldx #$27
-	ldy #$1b
-	jsr draw_circle
+  lda ZP_FGCOLOR
+  sta ZP_BGCOLOR
+  lda #0
+  sta ZP_FGCOLOR
+  lda #80
+  jsr draw_circle
 
-	jsr wait_button
+  jsr wait_button
 
-	lda ZP_FGCOLOR
-	sta ZP_BGCOLOR
-	lda #4
-	sta ZP_FGCOLOR
-	lda #64
-	ldx #$31
-	ldy #$16
-	jsr draw_circle
+  lda ZP_FGCOLOR
+  sta ZP_BGCOLOR
+  lda #4
+  sta ZP_FGCOLOR
+  lda #64
+  jsr draw_circle
 
-	jsr wait_button
+  jsr wait_button
 
-	lda ZP_FGCOLOR
-	sta ZP_BGCOLOR
-	lda #5
-	sta ZP_FGCOLOR
-	lda #48
-	ldx #$41
-	ldy #$10
-	jsr draw_circle
+  lda ZP_FGCOLOR
+  sta ZP_BGCOLOR
+  lda #5
+  sta ZP_FGCOLOR
+  lda #48
+  jsr draw_circle
 
-	jsr wait_button
+  jsr wait_button
 
-	lda ZP_FGCOLOR
-	sta ZP_BGCOLOR
-	lda #1
-	sta ZP_FGCOLOR
-	lda #32
-	ldx #$61
-	ldy #$0b
-	jsr draw_circle
+  lda ZP_FGCOLOR
+  sta ZP_BGCOLOR
+  lda #1
+  sta ZP_FGCOLOR
+  lda #32
+  jsr draw_circle
 
-	jsr wait_button
+  jsr wait_button
 
-	lda ZP_FGCOLOR
-	sta ZP_BGCOLOR
-	lda #0
-	sta ZP_FGCOLOR
-	lda #16
-	ldx #$c1
-	ldy #$05
-	jsr draw_circle
+  lda ZP_FGCOLOR
+  sta ZP_BGCOLOR
+  lda #0
+  sta ZP_FGCOLOR
+  lda #16
+  jsr draw_circle
 
-	jsr wait_button
-	jsr wait_button
+  jsr wait_button
+  jsr wait_button
 
-	lda #5
-	sta ZP_BGCOLOR
-	lda #1
-	sta ZP_FGCOLOR
-	lda #32
-	ldx #$61
-	ldy #$0b
-	jsr draw_circle
+  lda #5
+  sta ZP_BGCOLOR
+  lda #1
+  sta ZP_FGCOLOR
+  lda #32
+  jsr draw_circle
 
-	lda ZP_FGCOLOR
-	sta ZP_BGCOLOR
-	lda #5
-	sta ZP_FGCOLOR
+  lda ZP_FGCOLOR
+  sta ZP_BGCOLOR
+  lda #5
+  sta ZP_FGCOLOR
 
-	ldx #70
-	stx DRAWSTRING_X
-	ldy #86
-	sty DRAWSTRING_Y
-	jsr drawstringimm
-	.asciiz "HAPPY"
+  ldx #70
+  stx DRAWSTRING_X
+  ldy #86
+  sty DRAWSTRING_Y
+  jsr drawstringimm
+  .asciiz "HAPPY"
 
-	ldx #68
-	stx DRAWSTRING_X
-	ldy #96
-	sty DRAWSTRING_Y
-	jsr drawstringimm
-	.asciiz "PI DAY"
+  ldx #68
+  stx DRAWSTRING_X
+  ldy #96
+  sty DRAWSTRING_Y
+  jsr drawstringimm
+  .asciiz "PI DAY"
 
-	ldx #72
-	stx DRAWSTRING_X
-	ldy #106
-	sty DRAWSTRING_Y
-	jsr drawstringimm
-	.asciiz "2021"
+  ldx #72
+  stx DRAWSTRING_X
+  ldy #106
+  sty DRAWSTRING_Y
+  jsr drawstringimm
+  .asciiz "2021"
 
-	jsr wait_button
+  jsr wait_button
 
-	lda #1
-	sta ZP_RANDOMCOLOURS_FLAG
+  lda #1
+  sta ZP_RANDOMCOLOURS_FLAG
 
 .loop:
-	ldx #70
-	stx DRAWSTRING_X
-	ldy #86
-	sty DRAWSTRING_Y
-	jsr drawstringimm
-	.asciiz "HAPPY"
+  ldx #70
+  stx DRAWSTRING_X
+  ldy #86
+  sty DRAWSTRING_Y
+  jsr drawstringimm
+  .asciiz "HAPPY"
 
-	ldx #68
-	stx DRAWSTRING_X
-	ldy #96
-	sty DRAWSTRING_Y
-	jsr drawstringimm
-	.asciiz "PI DAY"
+  ldx #68
+  stx DRAWSTRING_X
+  ldy #96
+  sty DRAWSTRING_Y
+  jsr drawstringimm
+  .asciiz "PI DAY"
 
-	ldx #72
-	stx DRAWSTRING_X
-	ldy #106
-	sty DRAWSTRING_Y
-	jsr drawstringimm
-	.asciiz "2021"
+  ldx #72
+  stx DRAWSTRING_X
+  ldy #106
+  sty DRAWSTRING_Y
+  jsr drawstringimm
+  .asciiz "2021"
 
-	jmp .loop
-
-
-mulf:
-.ARG1 = $30
-.ARG2 = $31
-.RESULT = $0
-	; Multiply A by X, shift right 4, truncate
-	sta .ARG1
-	stx .ARG2
-	lda #0
-	sta .RESULT
-	sta .RESULT+1
-	ldx #8
-.loop
-	asl .ARG1
-	bcc .nocarry
-	clc
-	lda .RESULT
-	adc .ARG2
-	sta .RESULT
-	lda .RESULT+1
-	adc #0
-	sta .RESULT+1
-
-.nocarry
-	asl .RESULT
-	rol .RESULT+1
-	dex
-	bne .loop
-
-	lda .RESULT+1
-	asl .RESULT
-	rol
-	asl .RESULT
-	rol
-
-	rts
+  jmp .loop
 
 
 draw_circle:
-.CENTREX = 80
-.CENTREY = 100
+  ; Radius in A
+  ;
+  ; New version using Midpoint Algorithm
+  ; Thanks to Gordon Henderson for the pointer!
+.Y1 = $90   ; Y counting up
+.Y2 = $91   ; Y counting down
+.X1 = $92   ; X at Y
+.X2 = $93   ; X at YY
+.RES = $94  ; two bytes
 
-.YPOS = $10
-.WIDTH = $11
-.LEFT = $12
-.RIGHT = $13
-.HALFWIDTH = $14
-.MUL1 = $15
-.MUL2 = $16
+  sta .X1
+  sta .RES
+  sec
+  sbc #1
+  sta .Y2
+  lda #0
+  sta .Y1
+  sta .X2
+  sta .RES+1
 
-	sta .YPOS
-	stx .MUL1
-	sty .MUL2
+.loop
+  ; hline Y1 -X1 X1
+  ; hline -Y1 -X1 X1
+  lda .Y1
+  ldx .X1
+  jsr .dolines
 
-.yloop:
-	ldx .YPOS
-	bne .notdoneyet
-	rts
+  ; x2 += 1
+  inc .X2
 
-.notdoneyet
-	dex
-	stx .YPOS
+  ; residue -= y1
+  sec
+  lda .RES
+  sbc .Y1
+  sta .RES
+  lda .RES+1
+  sbc #0
+  sta .RES+1
 
-	lda .MUL1
-	jsr mulf
-	tax
-	lda squareroots,x   ; sqrt(96*96 - YPOS*YPOS)
+  ; y1 += 1
+  inc .Y1
 
-	ldx .MUL2
-	jsr mulf
+  ; residue -= y1
+  sec
+  lda .RES
+  sbc .Y1
+  sta .RES
+  lda .RES+1
+  sbc #0
+  sta .RES+1
 
-	sta .WIDTH
-	lsr
-	sta .HALFWIDTH
+  ; if residue < 0:
+  bpl .notneg
 
-	sec
-	lda #.CENTREX
-	sbc .HALFWIDTH
-	sta .LEFT
+  ; residue += x1
+  clc
+  lda .RES
+  adc .X1
+  sta .RES
+  lda .RES+1
+  adc #0
+  sta .RES+1
 
-	lda .YPOS
-	clc
-	adc #.CENTREY
-	jsr vram_openline
-	jsr .doline
+  ; x1 -= 1
+  dec .X1
 
-	sec
-	lda #.CENTREY
-	sbc .YPOS
-	jsr vram_openline
-	jsr .doline
+  ; residue += x1
+  clc
+  lda .RES
+  adc .X1
+  sta .RES
+  lda .RES+1
+  adc #0
+  sta .RES+1
 
-	jmp .yloop
+  ; hline Y2 -X2 X2
+  ; hline -Y2 -X2 X2
+  lda .Y2
+  ldx .X2
+  jsr .dolines
 
+  dec .Y2
 
-.doline
+.notneg
+  jsr waitvsync
+  lda .Y2
+  cmp .Y1
+  bpl .loop
 
-	lda .WIDTH
-	and #1
-	beq .nooddpixels
+  rts
 
-	ldy .LEFT
-	dey
-	lda ZP_BGCOLOR
-	asl
-	ora ZP_FGCOLOR
-	ora #BITS_DEFAULT
-	sta (ZP_PTR),y
+.dolines
+.TEMP_Y = $a0
+  ; A = Y coord
+  ; X = X coord
+  ;
+  ; Draw two horizontal lines - one at +Y,
+  ; one at -Y - from -X to +X
 
-	clc
-	lda #.CENTREX
-	adc .HALFWIDTH
-	tay
-	lda ZP_FGCOLOR
-	asl
-	ora ZP_BGCOLOR
-	ora #BITS_DEFAULT
-	sta (ZP_PTR),y
+  sta .TEMP_Y
+
+  txa
+  lsr
+  sta .HLINE_HW
+
+  eor #$ff
+  sec ;+1
+  adc #80
+  sta .HLINE_X
+
+  txa
+  and #1
+  sta .HLINE_ODD
+
+  sec ;+1
+  lda #100
+  adc .TEMP_Y
+  jsr .hline
+
+  sec
+  lda #100
+  sbc .TEMP_Y
+
+.hline
+.HLINE_ODD = $a1
+.HLINE_X = $a2
+.HLINE_HW = $a3
+  jsr vram_openline
+
+  lda .HLINE_ODD
+  beq .nooddpixels
+
+  ldy .HLINE_X
+  dey
+  lda ZP_BGCOLOR
+  asl
+  ora ZP_FGCOLOR
+  ora #BITS_DEFAULT
+  sta (ZP_PTR),y
+
+  lda .HLINE_HW
+  asl
+  clc
+  adc .HLINE_X
+  tay
+  lda ZP_FGCOLOR
+  asl
+  ora ZP_BGCOLOR
+  ora #BITS_DEFAULT
+  sta (ZP_PTR),y
 
 .nooddpixels
-	lda ZP_FGCOLOR
-	asl
-	ora ZP_FGCOLOR
-	ora #BITS_DEFAULT
+  lda ZP_FGCOLOR
+  asl
+  ora ZP_FGCOLOR
+  ora #BITS_DEFAULT
 
-	ldy .LEFT
-	ldx .HALFWIDTH
+  ldy .HLINE_X
+  ldx .HLINE_HW
 .wloop:
-	sta (ZP_PTR),y
-	iny
-	sta (ZP_PTR),y
-	iny
-	dex
-	bne .wloop
+  sta (ZP_PTR),y
+  iny
+  sta (ZP_PTR),y
+  iny
+  dex
+  bne .wloop
 
-	rts
-
+  rts
 
 
 wait_button:
@@ -390,37 +405,37 @@ drawstring:
 .TEMP_LOOPCOUNT = $32
 
   pha
-	txa
-	pha
-	tya
-	pha
+  txa
+  pha
+  tya
+  pha
 
   ldy #0
 .loop:
   lda (printmessage_bufferptr),y
   beq .endloop
 
-	sty .TEMP_LOOPCOUNT
+  sty .TEMP_LOOPCOUNT
 
-	ldx DRAWSTRING_X
-	ldy DRAWSTRING_Y
-	jsr vid_drawchar
+  ldx DRAWSTRING_X
+  ldy DRAWSTRING_Y
+  jsr vid_drawchar
 
-	ldx DRAWSTRING_X
-	inx
-	inx
-	inx
-	inx
-	stx DRAWSTRING_X
+  ldx DRAWSTRING_X
+  inx
+  inx
+  inx
+  inx
+  stx DRAWSTRING_X
 
-	ldy ZP_RANDOMCOLOURS_FLAG
-	beq .notrandomcolours
+  ldy ZP_RANDOMCOLOURS_FLAG
+  beq .notrandomcolours
 
-	jsr nextcolour
+  jsr nextcolour
 
 .notrandomcolours
 
-	ldy .TEMP_LOOPCOUNT
+  ldy .TEMP_LOOPCOUNT
   iny
   jmp .loop
 
@@ -433,8 +448,8 @@ drawstring:
   adc #0
   sta printmessage_bufferptr+1
 
-	pla
-	tay
+  pla
+  tay
   pla
   tax
   pla
@@ -442,11 +457,11 @@ drawstring:
 
 
 nextcolour:
-	jsr random
-	and #5
-	cmp #1
-	beq nextcolour
-	sta ZP_FGCOLOR
+  jsr random
+  and #5
+  cmp #1
+  beq nextcolour
+  sta ZP_FGCOLOR
   jmp waitvsync
 
 waitvsync
